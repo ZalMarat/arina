aa35f88b230f7efffbf9b49ca3586c46756be0fb
ZalMarat <despaircaptain99@gmail.com>
2026-07-13 23:05:40 +0300

    сделал компонент услуг и добавил их (частично) на главную страницу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
 import Button from "@/components/button";
+import ServicesCategory from "@/components/services";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-inter">
       <main className="">
-        <section className="bg-[url('/img/1.webp')] clip w-[2300] h-[500]">
+        <section className="bg-[url('/img/1.webp')] clip w-[2300] h-[500] flex">
           <div className="flex flex-col gap-6 mx-5 my-5">
-            <h1 className="">ТУТ БУДЕТ АДЕКВАТНЫЙ ТЕКСТ</h1>
+            <h1 className="font-bold">ТУТ БУДЕТ АДЕКВАТНЫЙ ТЕКСТ</h1>
             <Button>Записаться</Button>
+          </div>
+        </section>
+
+        {/* <Image src={}></Image> */}
+        <section className="mx-5 my-5 ">
+          <div className="content-center">
+            <h1 className="text-4xl font-bold">
+              ПРИГЛАШАЮ НА ОЧНЫЕ И ОНЛАЙН КОНСУЛЬТАЦИИ
+            </h1>
+          </div>
+          <div>
+            <ServicesCategory title="Чоловіки">
+              Готова підтримати Вас у складний момент та допомогти повернути
+              радість життя.
+            </ServicesCategory>
           </div>
         </section>
       </main>
---
+++ src/components/services.tsx
@@ -0,0 +1,19 @@
+interface Services {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function ServicesCategory({ title, children }: Services) {
+  return (
+    <section className="flex h-20 overflow-hidden rounded-2xl border border-[#F3C98A] bg-white font-inter">
+      <div className="flex w-32 items-center justify-center bg-[#FFD28C]">
+        <div>
+          <span className="text-xl font-bold text-white">{title}</span>
+        </div>
+      </div>
+      <div className="flex flex-1 items-center justify-center px-6 text-center text-sm font-semibold text-[#1A2E5A] ">
+        {children}
+      </div>
+    </section>
+  );
+}
